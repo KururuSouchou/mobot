a04f8940ea2dafd5c407052a8fa9190a17899022
966 <stupidfool9413@gmail.com>
2017-06-11 15:25:56 +0800

    register legion command

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,7 +4,7 @@ from .start_command_handler import start_command_handler,\
     person_command_handler, fuck_person_command_handler,\
     location_command_handler, fuck_location_command_handler,\
     add_game_command_handler, remove_game_command_handler,\
-    price_command_handler
+    price_command_handler, legion_command_handler
 
 HANDLERS = (
     dealhigh_handler,
@@ -16,5 +16,6 @@ HANDLERS = (
     add_game_command_handler,
     remove_game_command_handler,
     price_command_handler,
+    legion_command_handler
 )
 __all__ = HANDLERS
